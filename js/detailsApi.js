d52e4300073f1b5f80cc0d8dc70c24ffa5b32f5a
Mark <msheadshot20@gmail.com>
2022-05-26 23:43:02 -0500

    Latest

--- js/detailsApi.js
+++ js/detailsApi.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
       method: "GET",
       headers: {
         "X-RapidAPI-Host": "aerisweather1.p.rapidapi.com",
-        "X-RapidAPI-Key": "redacted",
+        "X-RapidAPI-Key": "39125ad083msha052ceab43936bbp11b52bjsne930007ec12f",
       },
     };
 
